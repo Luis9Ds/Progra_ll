224f5611bd0ccee1f0283028a8310ebb87f3e276
Luis9Ds <diegopicadobe@gmail.com>
2023-09-20 21:23:46 -0600

    asd

--- Projectos/Peliculas/peliculas/urls.py
+++ Projectos/Peliculas/peliculas/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros/', views.nosotros, name='nosotros'),
     path('peliculas/', views.peliculas, name='peliculas'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #Esto es para agregar imagenes
--- Projectos/Peliculas/peliculas/views.py
+++ Projectos/Peliculas/peliculas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pelicula
 # Create your views here.
 
 def nosotros(request):
@@ -9,4 +10,5 @@ def inicio(request):
     return render(request, "paginas/inicio.html")
 
 def peliculas(request):
-    return render(request, "peliculas/index.html")
+    peliculas = Pelicula.objects.all()
+    return render(request, "peliculas/index.html", {"peliculas":peliculas})
